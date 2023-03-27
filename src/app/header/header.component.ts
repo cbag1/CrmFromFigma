0e70d37221373c155c7a33e1e000b9712c926349
cbag iatf <cbag-iatf@iatf.it>
2023-03-27 15:12:30 +0000

    wip integration first page

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -12,6 +12,9 @@ export class HeaderComponent implements OnInit {
   layout_block = 'assets/images/layout_block.png';
   group_chat= 'assets/images/group_chat.png';
   equalizer = 'assets/images/equalizer.png';
+  add_user = 'assets/images/Add-user.png';
+  avatar1 = 'assets/images/avatar1.png';
+  right= 'assets/images/right.png';
 
   constructor(private router: Router) {}
 
